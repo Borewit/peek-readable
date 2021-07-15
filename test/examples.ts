9984df90bdff835c47d51306579fb6e5191d3300
Borewit <borewit@users.noreply.github.com>
2021-07-15 08:46:11 +0200

    Buffer abstraction by using Uint8Array where possible

--- test/examples.ts
+++ test/examples.ts
@@ -1,48 +1,56 @@
 /* tslint:disable:no-console */
 
-import { assert, expect } from 'chai';
+import { assert } from 'chai';
 import * as fs from 'fs';
 import * as path from 'path';
-import { StreamReader } from '../lib';
+import { EndOfStreamError, StreamReader } from '../lib';
 
 describe('Examples', () => {
 
-  it('first example', () => {
+  it('first example', async () => {
 
     const readable = fs.createReadStream(path.join(__dirname, 'resources', 'JPEG_example_JPG_RIP_001.jpg'));
     const streamReader = new StreamReader(readable);
-
-    const buffer = Buffer.alloc(16);
-
-    return streamReader.read(buffer, 0, 16)
-      .then(bytesRead => {
-        assert.equal(bytesRead, 16);
-        // buf, contains bytesRead, which will be 16 if the end-of-stream has not been reached
-      });
+    const uint8Array = new Uint8Array(16);
+    const bytesRead = await streamReader.read(uint8Array, 0, 16);
+    assert.equal(bytesRead, 16);
   });
 
-  it('peek', () => {
+  it('End-of-stream detection', async () => {
+
+    const fileReadStream = fs.createReadStream(path.join(__dirname, 'resources', 'JPEG_example_JPG_RIP_001.jpg'));
+    const streamReader = new StreamReader(fileReadStream);
+    const uint8Array =  new Uint8Array(16);
+    assert.equal(await streamReader.read(uint8Array, 0, 16), 16);
+    try {
+      while(await streamReader.read(uint8Array, 0, 1) > 0);
+      assert.fail('Should throw EndOfStreamError');
+    } catch(error) {
+      assert.isOk(error instanceof EndOfStreamError, 'Expect `error` to be instance of `EndOfStreamError`');
+      if (error instanceof EndOfStreamError) {
+        console.log('End-of-stream reached');
+      }
+    }
+  });
+
+  it('peek', async () => {
 
     const fileReadStream = fs.createReadStream(path.join(__dirname, 'resources', 'JPEG_example_JPG_RIP_001.jpg'));
     const streamReader = new StreamReader(fileReadStream);
     const buffer = Buffer.alloc(20);
 
-    return streamReader.peek(buffer, 0, 3)
-      .then(bytesRead => {
-        if (bytesRead === 3 && buffer[0] === 0xFF && buffer[1] === 0xD8 && buffer[2] === 0xFF) {
-          console.log('This is a JPEG file');
-          return streamReader.read(buffer, 0, 20); // Read JPEG header
-        } else {
-          throw Error('Expected a JPEG file');
-        }
-      })
-      .then(bytesRead => {
-        if (bytesRead === 20) {
-          console.log('Got the JPEG header');
-        } else {
-          throw Error('Failed to read JPEG header');
-        }
-      });
+    let bytesRead = await streamReader.peek(buffer, 0, 3);
+    if (bytesRead === 3 && buffer[0] === 0xFF && buffer[1] === 0xD8 && buffer[2] === 0xFF) {
+      console.log('This is a JPEG file');
+    } else {
+      throw Error('Expected a JPEG file');
+    }
+    bytesRead = await streamReader.read(buffer, 0, 20); // Read JPEG header
+    if (bytesRead === 20) {
+      console.log('Got the JPEG header');
+    } else {
+      throw Error('Failed to read JPEG header');
+    }
   });
 
 });
