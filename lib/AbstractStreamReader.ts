e56c78124b07f392a85747db1087927f92928a0a
Borewit <Borewit@users.noreply.github.com>
2024-07-02 22:57:07 +0200

    Fix Web Stream implementation

---
+++ lib/AbstractStreamReader.ts
@@ -0,0 +1,98 @@
+import { Deferred } from './Deferred.js';
+import {EndOfStreamError, IStreamReader} from "./index.js";
+
+export interface IReadRequest {
+  buffer: Uint8Array,
+  offset: number,
+  length: number,
+  position?: number,
+  deferred: Deferred<number>
+}
+
+type isEndOfStream = () => boolean;
+
+export abstract class AbstractStreamReader implements IStreamReader {
+
+  /**
+   * Maximum request length on read-stream operation
+   */
+  protected maxStreamReadSize = 1 * 1024 * 1024;
+
+  protected endOfStream = false;
+
+  /**
+   * Store peeked data
+   * @type {Array}
+   */
+  protected peekQueue: Uint8Array[] = [];
+
+  /**
+   * Read ahead (peek) from stream. Subsequent read or peeks will return the same data
+   * @param uint8Array - Uint8Array (or Buffer) to store data read from stream in
+   * @param offset - Offset target
+   * @param length - Number of bytes to read
+   * @returns Number of bytes peeked
+   */
+  public async peek(uint8Array: Uint8Array, offset: number, length: number): Promise<number> {
+    const bytesRead = await this.read(uint8Array, offset, length);
+    this.peekQueue.push(uint8Array.subarray(offset, offset + bytesRead)); // Put read data back to peek buffer
+    return bytesRead;
+  }
+
+  public async read(buffer: Uint8Array, offset: number, length: number): Promise<number> {
+    if (length === 0) {
+      return 0;
+    }
+
+    let bytesRead = this.readFromPeekBuffer(buffer, offset, length);
+    bytesRead += await this.readRemainderFromStream(buffer, offset + bytesRead, length - bytesRead, () => false);
+    if (bytesRead === 0) {
+      throw new EndOfStreamError();
+    }
+    return bytesRead;
+  }
+
+  /**
+   * Read chunk from stream
+   * @param buffer - Target Uint8Array (or Buffer) to store data read from stream in
+   * @param offset - Offset target
+   * @param length - Number of bytes to read
+   * @returns Number of bytes read
+   */
+  protected readFromPeekBuffer(buffer: Uint8Array, offset: number, length: number): number {
+
+    let remaining = length;
+    let bytesRead = 0;
+    // consume peeked data first
+    while (this.peekQueue.length > 0 && remaining > 0) {
+      const peekData = this.peekQueue.pop(); // Front of queue
+      if (!peekData) throw new Error('peekData should be defined');
+      const lenCopy = Math.min(peekData.length, remaining);
+      buffer.set(peekData.subarray(0, lenCopy), offset + bytesRead);
+      bytesRead += lenCopy;
+      remaining -= lenCopy;
+      if (lenCopy < peekData.length) {
+        // remainder back to queue
+        this.peekQueue.push(peekData.subarray(lenCopy));
+      }
+    }
+    return bytesRead;
+  }
+
+  public async readRemainderFromStream(buffer: Uint8Array, offset: number, remaining: number, isEndOfStream: isEndOfStream): Promise<number> {
+
+    let bytesRead = 0;
+    // Continue reading from stream if required
+    while (remaining > 0 && !isEndOfStream()) {
+      const reqLen = Math.min(remaining, this.maxStreamReadSize);
+      const chunkLen = await this.readFromStream(buffer, offset + bytesRead, reqLen);
+      bytesRead += chunkLen;
+      if (chunkLen < reqLen)
+        break;
+      remaining -= chunkLen;
+    }
+    return bytesRead;
+  }
+
+  protected abstract readFromStream(buffer: Uint8Array, offset: number, length: number): Promise<number>;
+}
--- lib/WebStreamReader.ts
+++ lib/WebStreamReader.ts
@@ -1,51 +1,31 @@
-// @ts-ignore
 import { ReadableStreamBYOBReader, ReadableStream } from 'node:stream/web';
 import { EndOfStreamError } from './EndOfFileStream.js';
 export { EndOfStreamError } from './EndOfFileStream.js';
-
-import type { IStreamReader } from "./index.js";
+import { AbstractStreamReader } from "./AbstractStreamReader.js";
 
 /**
  * Read from a WebStream
  * Reference: https://nodejs.org/api/webstreams.html#class-readablestreambyobreader
  */
-export class WebStreamReader implements IStreamReader {
+export class WebStreamReader extends AbstractStreamReader {
   private reader: ReadableStreamBYOBReader;
-  private peekQueue: Uint8Array[] = [];
+  private eofStream = false;
 
   public constructor(stream: ReadableStream<Uint8Array>) {
+    super();
     this.reader = stream.getReader({ mode: 'byob' }) as ReadableStreamBYOBReader;
   }
 
-  public async peek(buffer: Uint8Array, offset: number, length: number): Promise<number> {
-    const bytesRead = await this.read(buffer, offset, length);
-    this.peekQueue.push(buffer.subarray(offset, offset + bytesRead));
-    return bytesRead;
-  }
+  protected async readFromStream(buffer: Uint8Array, offset: number, length: number): Promise<number> {
 
-  public async read(buffer: Uint8Array, offset: number, length: number): Promise<number> {
-    if (length === 0) {
-      return 0;
-    }
-
-    if (this.peekQueue.length > 0) {
-      let bytesRead = 0;
-      while (this.peekQueue.length > 0 && bytesRead < length) {
-        const peeked = this.peekQueue.shift()!;
-        const toCopy = Math.min(peeked.length, length - bytesRead);
-        buffer.set(peeked.subarray(0, toCopy), offset + bytesRead);
-        bytesRead += toCopy;
-        if (toCopy < peeked.length) {
-          this.peekQueue.unshift(peeked.subarray(toCopy));
-        }
-      }
-      return bytesRead;
+    if(this.eofStream) {
+      throw new EndOfStreamError();
     }
 
     const result = await this.reader.read(new Uint8Array(length));
 
     if (result.done) {
-      throw new EndOfStreamError();
+      this.eofStream = result.done;
     }
 
     if(result.value) {
