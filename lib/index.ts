fa5258bd0c2269177d642fa52c5af460a8984640
Borewit <Borewit@users.noreply.github.com>
2024-08-08 14:30:21 +0200

    Allow both DOM and Node.js web-byte-streams

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
 export { EndOfStreamError } from './EndOfStreamError.js';
 export { StreamReader } from './StreamReader.js';
-export { WebStreamReader } from './WebStreamReader.js';
+export { WebStreamReader, type AnyWebByteStream } from './WebStreamReader.js';
 export type { IStreamReader } from './AbstractStreamReader.js';
