d24b5de31a291af617f8cd0b8fbc90439c9b0cda
Borewit <Borewit@users.noreply.github.com>
2025-03-04 11:06:11 +0100

    Use DOM whatwg stream types in favour of conflicting Node.js types

--- lib/WebStreamReaderFactory.ts
+++ lib/WebStreamReaderFactory.ts
@@ -2,14 +2,14 @@ import type { ReadableStream as NodeReadableStream } from 'node:stream/web';
 import { WebStreamByobReader } from './WebStreamByobReader.js';
 import { WebStreamDefaultReader } from './WebStreamDefaultReader.js';
 
-export type AnyWebByteStream = NodeReadableStream<Uint8Array> | ReadableStream<Uint8Array> | ReadableStream;
+export type AnyWebByteStream = NodeReadableStream<Uint8Array> | ReadableStream<Uint8Array>;
 
 export function makeWebStreamReader(stream: AnyWebByteStream): WebStreamByobReader | WebStreamDefaultReader {
   try {
     const reader = stream.getReader({mode: "byob"});
     if (reader instanceof ReadableStreamDefaultReader) {
       // Fallback to default reader in case `mode: byob` is ignored
-      return new WebStreamDefaultReader(reader as ReadableStreamDefaultReader);
+      return new WebStreamDefaultReader(reader as ReadableStreamDefaultReader<Uint8Array>);
     }
     return new WebStreamByobReader(reader);
   } catch(error) {
